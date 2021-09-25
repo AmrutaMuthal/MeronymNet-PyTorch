380499aa05e282cd1728870808682dd75e2a7ab5
Amruta Muthal <amruta.muthal.1992@gmail.com>
2021-09-25 18:55:04 +0530

    Added GAT based encoder

--- src/components/AutoEncoder.py
+++ src/components/AutoEncoder.py
@@ -6,10 +6,15 @@ from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import to_dense_adj
 from torch_geometric.data import Data
 
-from Encoder import Encoder
-from Decoder import Decoder
+from components.Encoder import GCNEncoder, GATEncoder
+from components.Decoder import Decoder
 
-class AutoEncoder(nn.Module):
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        
+
+class GCNAutoEncoder(nn.Module):
     
     """ AutoEncoder module for Box-Vae
     """
@@ -24,19 +29,19 @@ class AutoEncoder(nn.Module):
                  hidden3=128
                 ):
         
-        super(AutoEncoder, self).__init__()
+        super(GCNAutoEncoder, self).__init__()
         self.latent_dims = latent_dims
         self.num_nodes = num_nodes
         self.num_obj_classes = num_obj_classes
-        self.encoder = Encoder(latent_dims,
-                               num_nodes,
-                               bbx_size,
-                               label_size,
-                               num_obj_classes,
-                               hidden1,
-                               hidden2,
-                               hidden3,
-                              )
+        self.encoder = GCNEncoder(latent_dims,
+                                  num_nodes,
+                                  bbx_size,
+                                  label_size,
+                                  num_obj_classes,
+                                  hidden1,
+                                  hidden2,
+                                  hidden3,
+                                 )
         
         self.decoder = Decoder(latent_dims,
                                num_nodes,
@@ -50,7 +55,7 @@ class AutoEncoder(nn.Module):
         batch_size = z_mean.shape[0]
         
         #sampling
-        epsilon = torch.normal(torch.zeros(z_logvar.shape)).cuda()
+        epsilon = torch.normal(torch.zeros(z_logvar.shape, device=device))
         z_latent = z_mean + epsilon*torch.exp(z_logvar)
         
         # conditioning
@@ -63,4 +68,57 @@ class AutoEncoder(nn.Module):
         
         return x_bbx, x_lbl, x_edge, class_pred, z_mean, z_logvar
 
+
+class GATAutoEncoder(nn.Module):
     
+    """ AutoEncoder module for Box-Vae
+    """
+    def __init__(self,
+                 latent_dims,
+                 num_nodes,
+                 bbx_size,
+                 num_obj_classes,
+                 label_size=1,
+                 hidden1=32,
+                 hidden2=16,
+                 hidden3=128
+                ):
+        
+        super(GATAutoEncoder, self).__init__()
+        self.latent_dims = latent_dims
+        self.num_nodes = num_nodes
+        self.num_obj_classes = num_obj_classes
+        self.encoder = GATEncoder(latent_dims,
+                                  num_nodes,
+                                  bbx_size,
+                                  label_size,
+                                  num_obj_classes,
+                                  hidden1,
+                                  hidden2,
+                                  hidden3,
+                                 )
+        
+        self.decoder = Decoder(latent_dims,
+                               num_nodes,
+                               bbx_size,
+                               num_obj_classes,
+                               label_size)
+        
+    def forward(self,E, X , nodes, obj_class):
+
+        z_mean, z_logvar = self.encoder(E, X, obj_class)
+        batch_size = z_mean.shape[0]
+        
+        #sampling
+        epsilon = torch.normal(torch.zeros(z_logvar.shape, device=device))
+        z_latent = z_mean + epsilon*torch.exp(z_logvar)
+        
+        # conditioning
+        nodes = torch.reshape(nodes,(batch_size,self.num_nodes))
+        obj_class = torch.reshape(obj_class,(batch_size,self.num_obj_classes))
+        conditioned_z = torch.cat([nodes, z_latent],dim=-1)
+        conditioned_z = torch.cat([obj_class, conditioned_z],dim=-1)
+        
+        x_bbx, x_lbl, x_edge, class_pred = self.decoder(conditioned_z)
+        
+        return x_bbx, x_lbl, x_edge, class_pred, z_mean, z_logvar
