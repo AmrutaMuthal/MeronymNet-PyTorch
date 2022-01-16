6ca4c11d426c1665db87366475a86b9bc52d8587
Amruta Muthal <amruta.muthal.1992@gmail.com>
2022-01-16 15:20:14 +0530

    Added dynamic margin for box IOU loss

--- src/losses/BoxVAE_losses.py
+++ src/losses/BoxVAE_losses.py
@@ -23,9 +23,10 @@ def adj_loss(pred_edge, true_edge, batch, num_nodes):
 def bbox_loss(pred_box, true_box):
     
     # IOU loss
-    smooth_1 = torch.tensor([0.2]).cuda()
-    smooth_2 = torch.tensor([1e-06]).cuda()
+    smooth_1 = torch.tensor([15]).cuda()
+    smooth_2 = torch.tensor([1e-08]).cuda()
     zero = torch.tensor([0.0]).cuda()
+    one = torch.tensor([1.0]).cuda()
     true_box = torch.reshape(true_box,pred_box.shape)
     mask = torch.where(torch.sum(true_box,dim=-1,keepdim=True)!=0,1.0,0.0)
     pred_box = mask*pred_box
@@ -37,9 +38,70 @@ def bbox_loss(pred_box, true_box):
     xB = torch.minimum(x2g, x2)
     yB = torch.minimum(y2g, y2)
     
-    interArea = (torch.maximum(zero, (xB - xA + smooth_1)) * torch.maximum(zero,(yB - yA + smooth_1)))
-    boxAArea = (torch.maximum(zero, (x2g - x1g + smooth_1)) * torch.maximum(zero,(y2g - y1g + smooth_1)))
-    boxBArea = (torch.maximum(zero, (x2 - x1 + smooth_1)) * torch.maximum(zero,(y2 - y1 + smooth_1)))
+    w, h = x2g-x1g, y2g-y1g
+    
+    interArea = (torch.maximum(zero,
+                               (xB - xA + torch.maximum(smooth_1*w,one))) 
+                 * torch.maximum(zero, (yB - yA +torch.maximum(smooth_1*h,one))))
+    boxAArea = (torch.maximum(zero, 
+                              (x2g - x1g + torch.maximum(smooth_1*w,one))) 
+                * torch.maximum(zero,
+                                (y2g - y1g +torch.maximum(smooth_1*h,one))))
+    boxBArea = (torch.maximum(zero, 
+                              (x2 - x1 + torch.maximum(smooth_1*w,one))) 
+                * torch.maximum(zero,
+                                (y2 - y1 + torch.maximum(smooth_1*h,one))))
+    unionArea = boxAArea + boxBArea - interArea + smooth_2
+    
+    iouk = interArea / unionArea
+    iou_loss = -torch.log(iouk + smooth_2)
+    iou_loss = torch.mean(iou_loss)#/torch.sum(mask)
+    
+    # Box regression loss
+    reg_loss = F.mse_loss(pred_box, true_box, reduction='none')
+    reg_loss = torch.mean(reg_loss,dim = -1)
+    reg_loss = torch.sum(reg_loss,dim = -1)
+    total_non_zero = torch.count_nonzero(reg_loss)
+    reg_loss = torch.sum(reg_loss)/(total_non_zero+1)
+    
+    # Pairwise box regression loss
+    pair_mse_true = torch.cdist(true_box, true_box)
+    pair_mse_pred = torch.cdist(pred_box, pred_box)
+    pair_loss = F.mse_loss(pair_mse_true, pair_mse_pred)
+    total_non_zero = torch.count_nonzero(torch.sum(pair_loss,dim=-1))
+    pair_loss = torch.sum(pair_loss)/(total_non_zero+1)
+    
+    return iou_loss + reg_loss + pair_loss
+
+def bbox_loss_hw(pred_box, true_box):
+    
+    # IOU loss
+    smooth_1 = torch.tensor([10]).cuda()
+    smooth_2 = torch.tensor([1e-08]).cuda()
+    zero = torch.tensor([0.0]).cuda()
+    one = torch.tensor([0.7]).cuda()
+    true_box = torch.reshape(true_box,pred_box.shape)
+    mask = torch.where(torch.sum(true_box,dim=-1,keepdim=True)!=0,1.0,0.0)
+    pred_box = mask*pred_box
+    xg, yg, wg, hg = torch.tensor_split(true_box, 4, dim=-1)
+    xp, yp, wp, hp = torch.tensor_split(pred_box, 4, dim=-1)
+    
+    xA = torch.maximum(xg, xp)
+    yA = torch.maximum(yg, yp)
+    xB = torch.minimum(xg+wg, xp+hp)
+    yB = torch.minimum(yg+hg, yp+hp)
+    
+    interArea = (torch.maximum(zero,
+                               (xB - xA + torch.maximum(smooth_1*wg,one))) 
+                 * torch.maximum(zero, (yB - yA +torch.maximum(smooth_1*hg,one))))
+    boxAArea = (torch.maximum(zero, 
+                              (wg + torch.maximum(smooth_1*wg,one))) 
+                * torch.maximum(zero,
+                                (hg +torch.maximum(smooth_1*hg,one))))
+    boxBArea = (torch.maximum(zero, 
+                              (wp + torch.maximum(smooth_1*wg,one))) 
+                * torch.maximum(zero,
+                                (hp + torch.maximum(smooth_1*hg,one))))
     unionArea = boxAArea + boxBArea - interArea + smooth_2
     
     iouk = interArea / unionArea
